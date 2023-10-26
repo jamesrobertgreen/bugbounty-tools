047ca01adb877de8be8cd5fd5d2bf56a5789f414
Jim Green <jrgreen@gmail.com>
2023-10-26 10:15:23 +0100

    use anew instead of redirection, as amass fails sometimes

--- recon/get-subdomains.sh
+++ recon/get-subdomains.sh
@@ -6,6 +6,9 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
+# Get the directory where the current script is located
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Extract the domain and output file from the parameters
 domain="$1"
 
@@ -21,14 +24,18 @@ mkdir $output_folder
 
 
 # Command to run with the given domain and output file
-chaos -d $domain| anew $output_file
+chaos -d $domain | anew  $output_file
 
-amass enum -passive -d $domain --config ../../bugbounty-private/config/amass.cfg | anew $output_file
+amass enum -passive -d $domain --config $script_dir/../../bugbounty-private/config/amass.cfg | anew $output_file
 
 
 # Check the exit status of the command
 if [ $? -eq 0 ]; then
   echo "Output saved to $output_file"
+  grep -ve "\*\." $output_file > $output_file-tidy
+
+  sh $script_dir/prepend-https.sh $output_file-tidy $output_file
+ # rm $output_file-tidy
 else
   echo "Command failed. Check the error message for details."
 fi
